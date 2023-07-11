5fb9fce73f71a489a835a8b799ead26c16e6103b
Alp Ates <aykut.ates@klarna.com>
2023-07-11 22:44:15 +0200

    fix: add loading states to actions
    Loading states are missing on row component.

--- src/features/Entries/Row/Row.tsx
+++ src/features/Entries/Row/Row.tsx
@@ -1,6 +1,6 @@
 import { ArrowDown, ArrowUp, Trash2 } from "lucide-react";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useEntryActions } from "../../../hooks/useEntryAction";
 import {
   Actions,
@@ -14,6 +14,7 @@ import {
   ContentInnerWrapper,
   ContentImage,
 } from "./styles";
+import { Spinner } from "../../../components/Spinner/Spinner";
 
 interface Props {
   id: string;
@@ -31,7 +32,12 @@ export const Row = ({
   imageUrl,
 }: Props) => {
   const { push } = useRouter();
-  const { handleRemoveEntry, handlePublishEntry } = useEntryActions();
+  const {
+    handleRemoveEntry,
+    handlePublishEntry,
+    isLoadingPublish,
+    isLoadingRemove,
+  } = useEntryActions();
 
   const handleRemoveClick = useCallback(
     () => handleRemoveEntry(id),
@@ -67,10 +73,14 @@ export const Row = ({
       </Content>
       <Actions>
         <ActionButton onClick={handlePublishClick}>
-          {isPublished ? <ArrowDown /> : <ArrowUp />}
+          {isLoadingPublish ? (
+            <Spinner />
+          ) : (
+            <>{isPublished ? <ArrowDown /> : <ArrowUp />}</>
+          )}
         </ActionButton>
         <ActionButton onClick={handleRemoveClick}>
-          <Trash2 />
+          {isLoadingRemove ? <Spinner /> : <Trash2 />}
         </ActionButton>
       </Actions>
     </Container>
